a263125aab769a9816ba31b41f94408a7241a113
h7seiji <helder_seiji@hotmail.com>
2020-06-09 17:21:36 -0300

    default set True

--- tests/install.py
+++ tests/install.py
@@ -1,5 +1,5 @@
 import addon_utils
 
 
-if addon_utils.enable("blenderkit", default_set=False, persistent=True, handle_error=None) is None:
+if addon_utils.enable("blenderkit", default_set=True, persistent=True, handle_error=None) is None:
     exit(1)
