f2cc0b9063964bcd9792bba6d8070c68f7cdc257
Fabio Nukui <fabio.nukui@gmail.com>
2020-06-11 01:43:23 -0300

    added logic to set models' origin to zero on uploads

--- upload_bg.py
+++ upload_bg.py
@@ -129,6 +129,40 @@ def upload_files(upload_data, files):
     return uploaded_all
 
 
+def get_parent_object():
+    obj = bpy.context.scene.objects[0]
+    while obj.parent is not None:
+        obj = obj.parent
+    return obj
+
+
+def set_origin_zero(coll):
+    parent = get_parent_object()
+    if parent.type == 'EMPTY':
+        parent.select_set(True)
+        bpy.ops.object.transform_apply()
+
+        list_children = list(parent.children)
+        for child in parent.children:
+            child.parent = None
+        parent.location = (0, 0, 0)
+
+        for child in list_children:
+            child.parent = parent
+    else:
+        bpy.context.view_layer.objects.active = parent
+        parent.select_set(True)
+
+        bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+
+
+def fix_objects_origin(objects, coll):
+    """Move a group of objects to the center of the XY plane at height zero.
+    Origin of parent object is set to (0, 0, 0)"""
+    utils.centralize(objects)
+    set_origin_zero(coll)
+
+
 if __name__ == "__main__":
 
 
@@ -157,6 +191,7 @@ if __name__ == "__main__":
                 for o in allobs:
                     g.objects.link(o)
                 bpy.context.scene.collection.children.link(g)
+                fix_objects_origin(allobs, g)
             if export_data['type'] == 'SCENE':
                 sname = export_data['scene']
                 main_source = append_link.append_scene(file_name=data['source_filepath'],
