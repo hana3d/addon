3ac13661690c17dde91195d6d6248fbd6e572e7c
Fabio Nukui <fabio.nukui@gmail.com>
2020-08-10 18:35:44 -0300

    removed intermediate path variables

--- paths.py
+++ paths.py
@@ -35,12 +35,6 @@ HANA3D_AUTH_LANDING = "/landing"
 HANA3D_SETTINGS_FILENAME = os.path.join(_presets, "hana3d.json")
 
 RENDER_FARM_URL = 'https://api.notrenderfarm.com/production'
-RENDER_FARM_USER = 'users'
-RENDER_FARM_UPLOAD = 'upload'
-RENDER_FARM_PROJECT = 'projects'
-RENDER_FARM_JOB = 'jobs'
-RENDER_FARM_JOB_START = 'start'
-RENDER_FARM_JOB_CANCEL = 'cancel'
 
 URL_HANA3D_MAIN = 'https://api.hana3d.com'
 URL_HANA3D_LOCAL = 'http://localhost:5000'
@@ -109,31 +103,31 @@ def get_auth_audience():
 
 
 def get_render_farm_user_url(email):
-    return f'{RENDER_FARM_URL}/{RENDER_FARM_USER}/?email={email}'
+    return f'{RENDER_FARM_URL}/users/?email={email}'
 
 
 def get_render_farm_upload_url():
-    return f'{RENDER_FARM_URL}/{RENDER_FARM_UPLOAD}/?extension=.blend'
+    return f'{RENDER_FARM_URL}/upload/?extension=.blend'
 
 
 def get_render_farm_project_url(user_id):
-    return f'{RENDER_FARM_URL}/{RENDER_FARM_USER}/{user_id}/{RENDER_FARM_PROJECT}'
+    return f'{RENDER_FARM_URL}/users/{user_id}/projects'
 
 
 def get_render_farm_job_url(project_id):
-    return f'{RENDER_FARM_URL}/{RENDER_FARM_PROJECT}/{project_id}/{RENDER_FARM_JOB}'
+    return f'{RENDER_FARM_URL}/projects/{project_id}/jobs'
 
 
 def get_render_farm_job_start_url(job_id):
-    return f'{RENDER_FARM_URL}/{RENDER_FARM_JOB}/{job_id}/{RENDER_FARM_JOB_START}'
+    return f'{RENDER_FARM_URL}/jobs/{job_id}/start'
 
 
 def get_render_farm_job_get_url(user_id, job_id):
-    return f'{RENDER_FARM_URL}/{RENDER_FARM_USER}/{user_id}/{RENDER_FARM_JOB}?id={job_id}'
+    return f'{RENDER_FARM_URL}/users/{user_id}/jobs?id={job_id}'
 
 
 def get_render_farm_job_cancel_url(job_id):
-    return f'{RENDER_FARM_URL}/{RENDER_FARM_JOB}/{job_id}/{RENDER_FARM_JOB_CANCEL}'
+    return f'{RENDER_FARM_URL}/jobs/{job_id}/cancel'
 
 
 def default_global_dict():
