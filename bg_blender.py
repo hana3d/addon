58abc46ca041a59bd94bdacfa7a7991f96703aef
h7seiji <helder_seiji@hotmail.com>
2020-10-24 00:59:46 -0300

    remove print

--- bg_blender.py
+++ bg_blender.py
@@ -67,8 +67,6 @@ def threadread(tcom: threadCom):
     while True:
         line = tcom.proc.stdout.readline()
         line = str(line)
-        if len(line) > 3:
-            print(line, len(line))
         start = line.find('progress{')
         if start > -1:
             end = line.rfind('}')
