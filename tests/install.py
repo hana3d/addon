53e9e3fa757b2b0bf2f62d74eb7db8b6023e858e
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2020-11-09 21:10:48 -0300

    Update test

--- tests/install.py
+++ tests/install.py
@@ -4,7 +4,7 @@ import addon_utils
 import bpy
 
 
-enable = addon_utils.enable("hana3d", default_set=True, persistent=True, handle_error=None)
+enable = addon_utils.enable("hana3d_production", default_set=True, persistent=True, handle_error=None)
 
 if enable is None:
     sys.exit(1)
