13b8a5f297e2c884ce9b9952d49d5db4b9769741
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2020-11-17 16:51:41 -0300

    Use -beta tags for dev branch

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -1194,8 +1194,8 @@ def skip_tag_function(self, tag):
     # ---- write any custom code here, return true to disallow version ---- #
     #
     # # Filter out e.g. if 'beta' is in name of release
-    # if 'beta' in tag.lower():
-    #     return True
+    if 'beta' in tag.lower():
+        return True
     # ---- write any custom code above, return true to disallow version --- #
 
     if self.include_branches is True:
