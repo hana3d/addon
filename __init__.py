b091546172b8b3be7c6765a5056565c229eda9a3
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2020-12-01 13:33:56 -0300

    Bump patch

--- __init__.py
+++ __init__.py
@@ -52,7 +52,7 @@ from .src.authentication.authentication import Authentication
 bl_info = {
     'name': 'Hana3D',
     'author': 'Vilem Duha, Petr Dlouhy, R2U',
-    'version': (0, 7, 9),
+    'version': (0, 7, 10),
     'blender': (2, 90, 0),
     'location': 'View3D > Properties > Hana3D',
     'description': 'Online Hana3D library (materials, models, scenes and more). Connects to the internet.',  # noqa: E501
