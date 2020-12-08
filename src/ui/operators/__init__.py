f5b25ef0e858ba3ed64cf619674bb414ee168e2f
Ivan Padalko <ivan.padalko@real2u.com.br>
2020-12-08 16:47:35 -0300

    Move asset bar operator to new files

--- src/ui/operators/__init__.py
+++ src/ui/operators/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 """Blender UI Operators."""
 
+from .asset_bar import AssetBarOperator
 from .default_names import DefaultNamesOperator
 from .run_asset_bar import RunAssetBarWithContext
 from .transfer_data import TransferHana3DData
