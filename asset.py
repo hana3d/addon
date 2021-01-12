81d39d24a766bc769f9de99a77615efb1c2bd8f5
Ivan Padalko <ivan.padalko@real2u.com.br>
2021-01-12 17:59:24 -0300

    Fix lint

--- asset.py
+++ asset.py
@@ -18,10 +18,10 @@
 
 import bpy
 
+from . import utils
 from .config import HANA3D_DESCRIPTION, HANA3D_NAME
 from .report_tools import execute_wrapper
 from .src.upload import upload
-from . import utils
 
 
 class ShareAsset(bpy.types.Operator):
