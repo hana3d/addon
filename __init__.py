eb542cb75959b199c2127e4b158e4b216b5a1b3f
Ivan Padalko <ivan.padalko@real2u.com.br>
2021-02-01 11:53:22 -0300

    Update version

--- __init__.py
+++ __init__.py
@@ -51,8 +51,8 @@ from .src.ui.operators import render_image
 bl_info = {
     'name': 'Hana3D',
     'author': 'Vilem Duha, Petr Dlouhy, R2U',
-    'version': (0, 8, 4),
-    'blender': (2, 90, 0),
+    'version': (0, 8, 5),
+    'blender': (2, 91, 0),
     'location': 'View3D > Properties > Hana3D',
     'description': 'Online Hana3D library (materials, models, scenes and more). Connects to the internet.',  # noqa: E501
     'warning': '',
