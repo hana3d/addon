66ce235f1fbd23b3acee13b4483ad0b918c90f33
h7seiji <helder_seiji@hotmail.com>
2021-02-01 18:58:45 -0300

    lint

--- src/libraries/libraries.py
+++ src/libraries/libraries.py
@@ -21,22 +21,23 @@ def get_libraries(props: 'UploadProps'):  # noqa: WPS210
     """
     libraries: List[dict] = []
     for library_name in props.libraries_list.keys():
-        if props.libraries_list[library_name].selected is True:
-            library_id = props.libraries_list[library_name].id_
-            library = {}
-            library.update({
-                'id': library_id,
-            })
-            if props.custom_props.keys():
-                custom_props = {}
-                for prop_name in props.custom_props.keys():
-                    prop_value = props.custom_props[prop_name]
-                    slug = props.custom_props_info[prop_name]['slug']
-                    prop_library_id = props.custom_props_info[prop_name]['library_id']
-                    if prop_library_id == library_id:
-                        custom_props.update({slug: prop_value})  # noqa: WPS220
-                library.update({'metadata': {'view_props': custom_props}})
-            libraries.append(library)
+        if props.libraries_list[library_name].selected is not True:
+            continue
+        library_id = props.libraries_list[library_name].id_
+        library = {}
+        library.update({
+            'id': library_id,
+        })
+        if props.custom_props.keys():
+            custom_props = {}
+            for prop_name in props.custom_props.keys():
+                prop_value = props.custom_props[prop_name]
+                slug = props.custom_props_info[prop_name]['slug']
+                prop_library_id = props.custom_props_info[prop_name]['library_id']
+                if prop_library_id == library_id:
+                    custom_props.update({slug: prop_value})
+            library.update({'metadata': {'view_props': custom_props}})
+        libraries.append(library)
     return libraries
 
 
@@ -52,20 +53,24 @@ def set_library_props(asset_data, asset_props):
     for asset_library in asset_data.libraries:
         library = libraries_list[asset_library['name']]
         library.selected = True
-        if 'metadata' in asset_library and asset_library['metadata'] is not None:
-            for view_prop in library.metadata['view_props']:
-                name = f'{library.name} {view_prop["name"]}'
-                slug = view_prop['slug']
-                if name not in asset_props.custom_props:
-                    asset_props.custom_props_info[name] = {
-                        'slug': slug,
-                        'library_name': library.name,
-                        'library_id': library.id_,
-                    }
-                if 'view_props' in asset_library['metadata'] and slug in asset_library['metadata']['view_props']:  # noqa: E501
-                    asset_props.custom_props[name] = asset_library['metadata']['view_props'][slug]
-                else:
-                    asset_props.custom_props[name] = ''
+        if 'metadata' in asset_library and asset_library['metadata'] is None:
+            continue
+        for view_prop in library.metadata['view_props']:
+            name = f'{library.name} {view_prop["name"]}'
+            slug = view_prop['slug']
+            if name not in asset_props.custom_props:
+                asset_props.custom_props_info[name] = {
+                    'slug': slug,
+                    'library_name': library.name,
+                    'library_id': library.id_,
+                }
+            if (
+                'view_props' in asset_library['metadata']
+                and slug in asset_library['metadata']['view_props']
+            ):
+                asset_props.custom_props[name] = asset_library['metadata']['view_props'][slug]
+            else:
+                asset_props.custom_props[name] = ''
 
 
 def update_libraries_list(props: 'Props', context: bpy.types.Context):
