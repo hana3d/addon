3b9fd9356a91d2ec5eeb56de287534af0d601f9f
h7seiji <helder_seiji@hotmail.com>
2021-02-02 14:13:18 -0300

    format

--- hana3d_types.py
+++ hana3d_types.py
@@ -96,9 +96,27 @@ search_asset_type_items = (
 )
 
 upload_asset_type_items = (
-    ('MODEL', 'Upload Model', f'Upload a model to {HANA3D_DESCRIPTION}', 'OBJECT_DATAMODE', 0),
-    ('SCENE', 'Upload Scene', f'Upload a scene to {HANA3D_DESCRIPTION}', 'SCENE_DATA', 1),
-    ('MATERIAL', 'Upload Material', f'Upload a material to {HANA3D_DESCRIPTION}', 'MATERIAL', 2),
+    (
+        'MODEL',
+        'Upload Model',
+        f'Upload a model to {HANA3D_DESCRIPTION}',
+        'OBJECT_DATAMODE',
+        0
+    ),
+    (
+        'SCENE',
+        'Upload Scene',
+        f'Upload a scene to {HANA3D_DESCRIPTION}',
+        'SCENE_DATA',
+        1
+    ),
+    (
+        'MATERIAL',
+        'Upload Material',
+        f'Upload a material to {HANA3D_DESCRIPTION}',
+        'MATERIAL',
+        2
+    ),
 )
 
 
