c271d290e33c701762a4747a1ee0d057d08f1641
Ivan Padalko <ivan.padalko@real2u.com.br>
2021-03-08 18:12:03 -0300

    Fix lint

--- bump_version.py
+++ bump_version.py
@@ -1,18 +1,23 @@
+"""Update addon version."""
 import argparse
 import fileinput
 import re
 
-import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("version")
+def _main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('version')
 
-args = parser.parse_args()
-major, minor, patch = args.version.split('.')
+    args = parser.parse_args()
+    major, minor, patch = args.version.split('.')
 
-version_regex = re.compile(r"'version': \(\d+, \d+, \d+\),")
-with fileinput.input(files=('hana3d/__init__.py'), inplace=True) as f:
-    for line in f:
-        if version_regex.search(line):
-            line = re.sub(r"\d+, \d+, \d+", f'{major}, {minor}, {patch}', line)
-        print(line, end='')
+    version_regex = re.compile(r"'version': \(\d+, \d+, \d+\),")
+    with fileinput.input(files=('hana3d/__init__.py'), inplace=True) as init_file:
+        for line in init_file:
+            if version_regex.search(line):
+                line = re.sub(r'\d+, \d+, \d+', f'{major}, {minor}, {patch}', line)
+            print(line, end='')  # noqa: WPS421
+
+
+if __name__ == '__main__':
+    _main()
