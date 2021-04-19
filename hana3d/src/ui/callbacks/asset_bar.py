500bc2c7414ccd5444346253aaa7c55e07faf7f8
Ivan Padalko <ivan.padalko@real2u.com.br>
2021-04-19 12:10:33 -0300

    Add log when skipping image

--- hana3d/src/ui/callbacks/asset_bar.py
+++ hana3d/src/ui/callbacks/asset_bar.py
@@ -403,6 +403,7 @@ def draw_callback2d_search(self, context):
 
                 max_size = max(img.size[0], img.size[1])
                 if max_size == 0:
+                    logging.error(f'Image with name {iname} has both sides equal to zero, skipping')
                     continue
 
                 width = int(ui_props.thumb_size * img.size[0] / max_size)
